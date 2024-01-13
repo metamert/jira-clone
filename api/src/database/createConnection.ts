65e216e1a1aac1edfc0cb7c189fb4226e07bebae
Orkun mert <orkunmertgs41@gmail.com>
2024-01-13 13:40:17 +0300

    setup

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -2,16 +2,17 @@ import { createConnection, Connection } from 'typeorm';
 
 import * as entities from 'entities';
 
-const createDatabaseConnection = (): Promise<Connection> =>
-  createConnection({
+const createDatabaseConnection = (): Promise<Connection> => {
+  return createConnection({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    host: 'localhost',
+    port: Number(5432),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     entities: Object.values(entities),
     synchronize: true,
   });
+};
 
 export default createDatabaseConnection;
